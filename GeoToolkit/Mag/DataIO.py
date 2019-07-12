4d824072e5da62708f9e3169f73e7c256b68aa88
Angeline Leow <aleow@minerva.local>
2019-07-12 11:49:00 -0700

    convert rad to deg for TDX and tiltAngle

--- GeoToolkit/Mag/DataIO.py
+++ GeoToolkit/Mag/DataIO.py
@@ -318,7 +318,7 @@ class dataGrid(object):
 
             self._tiltAngle = np.arctan2(
                 self.firstVertical, self.totalHorizontal
-            )
+            )*180/np.pi
 
         return self._tiltAngle
 
@@ -342,7 +342,7 @@ class dataGrid(object):
 
               self._TDXderivative = np.arctan2(
                 self.totalHorizontal, self.firstVertical
-            )
+            )*180/np.pi
 
           return self._TDXderivative
 
