f043231ceecb7be216990dea1730b20fd4c0397d
Dominique Fournier <dominiquef@mirageoscience.com>
2020-05-03 12:26:14 -0700

    Force scipy==1.3.1 on setup.py

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ setup(
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1.15.4',
-        'scipy>=0.13',
+        'numpy==1.15.4',
+        'scipy==1.3.1',
         'tqdm',
         'matplotlib',
         'geopandas',
